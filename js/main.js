219cac0c6b082c7457cb703c1a64aa02e966e067
gbrian <gitegob7@gmail.com>
2022-04-03 00:04:09 +0200

    init 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const apiUrl = "http://localhost:8080/api/v1";
 
 const apiRequest = async (method, endpoint, body) => {
-  console.log(body);
   const response = await fetch(`${apiUrl}${endpoint}`, {
     method,
     headers: {
